869ef36d946f0e3edd99625517f0d45c605e53ea
hugosenari <hugosenari@gmail.com>
2022-03-12 01:15:09 -0300

    chore: re index cliptomania

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''.NET-inspired lightweight clipboard library'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-cliptomania-master.flake = false;
+  inputs.src-cliptomania-master.owner = "Guevara-chan";
+  inputs.src-cliptomania-master.ref   = "refs/heads/master";
+  inputs.src-cliptomania-master.repo  = "Cliptomania";
+  inputs.src-cliptomania-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-cliptomania-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-cliptomania-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
